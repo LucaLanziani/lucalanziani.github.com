8723422f0540e10206f3ad456c9e9cb35f155d4a
Luca Lanziani <luca@lanziani.com>
2015-01-02 15:02:56 +0000

    Shebang and cleans file before regenerate them

--- scripts/gen_index_min.sh
+++ scripts/gen_index_min.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 DEV_INDEX=index_dev.html
 
-rm js/libs.min.js
+echo > ./js/libs.min.js
 for i in $(awk '/src.*bower.*js/ { match($0, /src=['\''"][^'\''"]+['\''"]/); print substr($0, RSTART+5,RLENGTH-6) }' $DEV_INDEX);
 do (cat "${i}"; echo) >> ./js/libs.min.js
 done
 
-rm css/libs.min.css
+echo > css/libs.min.css
 for i in $(awk '/href.*bower.*css/ { match($0, /href=['\''"][^'\''"]+['\''"]/); print substr($0, RSTART+6,RLENGTH-7) }' $DEV_INDEX);
 do (cat "${i}"; echo) >> ./css/libs.min.css
 done
